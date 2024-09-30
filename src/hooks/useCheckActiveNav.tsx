4135fb85b499f5d0fabd1e35da6cc6da3b85906b
Your Name <you@example.com>
2024-09-30 10:08:15 +0700

    init

--- src/hooks/useCheckActiveNav.tsx
+++ src/hooks/useCheckActiveNav.tsx
@@ -5,7 +5,7 @@ export default function useCheckMenuItemActiveNav() {
 
   const checkActiveNav = (nav: string) => {
     const pathArray = pathname.split("/").filter((item) => item !== "");
-
+    console.log("pathArray", pathArray);
     if (nav === "/" && pathArray.length < 1) return true;
 
     return pathArray.includes(nav.replace(/^\//, ""));
